f51df50179ed995bcdf9dfa6df1a7e13d00e03bb
juno-junho <ssmm0205@naver.com>
2023-03-24 12:46:38 +0900

    feat(junolog): project setup

---
+++ src/main/java/com/junolog/JunologApplication.java
@@ -0,0 +1,13 @@
+package com.junolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunologApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunologApplication.class, args);
+    }
+
+}
